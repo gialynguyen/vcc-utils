9bb19068758b63c9e5dc83c3f33c72f022090aef
gialynguyen <emthanchet@gmail.com>
2021-04-30 14:28:13 +0700

    feat(is-date): add method isValidDate

--- src/utils/isDate.ts
+++ src/utils/isDate.ts
@@ -8,3 +8,8 @@ export function isDate(value: any): boolean {
     toString.call(value) == "[object Date]"
   );
 }
+
+export function isValidDate(value: any): boolean {
+  const tryParseDate = new Date(value);
+  return isDate(tryParseDate) && !isNaN(tryParseDate.getTime());
+}
